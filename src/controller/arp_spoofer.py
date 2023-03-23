7260957122a7c10c89e273760c6560a9890f7709
victorsanantonio <victorsanantonio@hotmail.com>
2023-03-23 14:01:23 +0100

    requirements.txt added

--- src/controller/arp_spoofer.py
+++
@@ -1,8 +0,0 @@
-#TODO: A requirements.txt
-#TODO: A markdown manual explaining:
-#      How to install (Installation)
-#      How to use (Usage)
-#      Funtionalities
-#      Codification
-#      Libraries
-#      Paradigm (Classes)
